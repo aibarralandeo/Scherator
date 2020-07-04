399035ba590c67197fae40a016abf1d7a70bdb62
Adolfo F. Ibarra Landeo <aibarralandeo@outlook.com>
2020-07-03 19:59:47 -0500

    Basic structure and icons created

---
+++ Scherator/Scherator.Client.Desktop/ViewModels/RelayCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scherator.Client.Desktop.ViewModels
+{
+    /// <summary>
+    /// A basic command that runs an action
+    /// </summary>
+    public class RelayCommand
+    {
+    }
+}
